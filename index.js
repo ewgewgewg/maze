cb28dbc1b9d039842c2b4ad2636e42e3b34b7ed4
unknown <ed.w.greenberg@gmail.com>
2020-10-31 15:19:12 -0400

    restart functionality

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
 const prompt = require("prompt-sync")();
 const _ = require("lodash");
-const { movement, aliasFunction } = require("./utils");
+const { movement, aliasFunction, goCondition } = require("./utils");
 
-let welcome = prompt(
-  "Welcome to the Dungeon! Would you like to ENTER? "
-).toUpperCase();
+let play = true;
 
-while (welcome !== "ENTER") {
-  welcome = prompt(
-    "You are floating in limbo. Would you like to ENTER? "
+const game = () => {
+  const endCriteria = [false, false];
+
+  let welcome = prompt(
+    "Welcome to the Dungeon! Would you like to ENTER? "
   ).toUpperCase();
-}
 
-const dungeon = [[], [], [], [], []].map((a) => ["x", "x", "x", "x", "x"]);
-let playerLocation = [0, 0],
-  monsterLocation = [4, 0];
+  while (welcome !== "ENTER") {
+    welcome = prompt(
+      "You are floating in limbo. Would you like to ENTER? "
+    ).toUpperCase();
+  }
 
-while (
-  (playerLocation[0] !== monsterLocation[0] ||
-    playerLocation[1] !== monsterLocation[1]) &&
-  (playerLocation[0] !== 4 || playerLocation[1] !== 4)
-) {
+  const dungeon = [[], [], [], [], []].map((a) => ["x", "x", "x", "x", "x"]);
+  let playerLocation = [0, 0, 0, 0],
+    monsterLocation = [4, 0, 4, 0];
+
+  while (goCondition(playerLocation, [monsterLocation])) {
+    const map = _.cloneDeep(dungeon);
+    map[playerLocation[0]][playerLocation[1]] = "o";
+    map[monsterLocation[0]][monsterLocation[1]] = "m";
+    console.log(map);
+    let playerDirection = prompt("LEFT, RIGHT, DOWN, OR UP? ");
+    if (playerDirection.toUpperCase() === "QUIT") {
+      endCriteria[1] = true;
+    }
+    playerDirection = aliasFunction(playerDirection);
+    playerLocation = movement(playerDirection, playerLocation);
+    monsterLocation = movement("RANDOM", monsterLocation);
+  }
+
   const map = _.cloneDeep(dungeon);
   map[playerLocation[0]][playerLocation[1]] = "o";
   map[monsterLocation[0]][monsterLocation[1]] = "m";
   console.log(map);
-  let playerDirection = prompt("LEFT, RIGHT, DOWN, OR UP? ");
-  playerDirection = aliasFunction(playerDirection);
-  playerLocation = movement(playerDirection, playerLocation);
-  monsterLocation = movement("RANDOM", monsterLocation);
+
+  if (
+    monsterLocation[0] === playerLocation[0] &&
+    monsterLocation[1] === playerLocation[1]
+  ) {
+    endCriteria[0] = true;
+  }
+
+  return endCriteria;
+};
+
+while (play) {
+  let [eaten, quit] = game();
+  if (eaten) {
+    console.log("You have been eaten!");
+  } else console.log("You have reached the end of the dungeon!");
+
+  if (quit) {
+    break;
+  }
+
+  let restart = prompt(
+    "Would you like to RESTART your journey? "
+  ).toUpperCase();
+  if (restart !== "RESTART") {
+    play = false;
+  }
 }
-
-const map = _.cloneDeep(dungeon);
-map[playerLocation[0]][playerLocation[1]] = "o";
-map[monsterLocation[0]][monsterLocation[1]] = "m";
-console.log(map);
-
-if (JSON.stringify(monsterLocation) === JSON.stringify(playerLocation)) {
-  console.log("You have been eaten!");
-} else console.log("You have reached the end of the dungeon!");
--- utils/index.js
+++ utils/index.js
@@ -13,15 +13,15 @@ const movement = (direction, unit) => {
 
   switch (realDirection) {
     case "RIGHT":
-      return [unit[0], Math.min(unit[1] + 1, 4)];
+      return [unit[0], Math.min(unit[1] + 1, 4), unit[0], unit[1]];
     case "LEFT":
-      return [unit[0], Math.max(unit[1] - 1, 0)];
+      return [unit[0], Math.max(unit[1] - 1, 0), unit[0], unit[1]];
     case "DOWN":
-      return [Math.min(unit[0] + 1, 4), unit[1]];
+      return [Math.min(unit[0] + 1, 4), unit[1], unit[0], unit[1]];
     case "UP":
-      return [Math.max(unit[0] - 1, 0), unit[1]];
+      return [Math.max(unit[0] - 1, 0), unit[1], unit[0], unit[1]];
     case "QUIT":
-      return [4, 4];
+      return [4, 4, unit[0], unit[1]];
     default:
       return unit;
   }
@@ -36,4 +36,20 @@ const aliasFunction = (input) => {
   return input;
 };
 
-module.exports = { movement, aliasFunction };
+const goCondition = (playerLocation, enemyLocations) => {
+  let go = true;
+
+  for (let el of enemyLocations) {
+    if (el[0] === playerLocation[0] && el[1] === playerLocation[1]) {
+      go = false;
+    }
+  }
+
+  if (4 === playerLocation[0] && 4 === playerLocation[1]) {
+    go = false;
+  }
+
+  return go;
+};
+
+module.exports = { movement, aliasFunction, goCondition };
